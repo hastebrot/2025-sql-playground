4b002f9d05e71b684b49c2def57151977b92865a
Benjamin Gudehus <hastebrot@gmail.com>
2025-01-30 05:30:31 +0100

    chore: Update to bun 1.2. (#1)

--- sources/run-bunsql.ts
+++ sources/run-bunsql.ts
@@ -1,9 +1,15 @@
-import { postgres } from "bun:sql";
+import { SQL } from "bun";
 
 if (import.meta.main) {
-  const sql = postgres("postgres://postgres@localhost:5432");
+  const sql = new SQL("postgres://postgres@localhost:5432");
   const res = await sql`select 1 as x`;
   console.log(res);
 
   await sql.close();
 }
+
+type NonEmptyArray<T> = [T, ...T[]];
+
+const okay: NonEmptyArray<number> = [1, 2];
+const alsoOkay: NonEmptyArray<number> = [1];
+const err: NonEmptyArray<number> = []; // error!
